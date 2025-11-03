2ff61331711361ef356010570790b7877e60d2d2
Patryk Rogala <patryk@astarium.pl>
2025-11-03 14:50:53 +0100

    Replace action_policy with pundit for authorization management
    - Updated Gemfile to include the `pundit` gem and removed `action_policy`.
    - Modified ApplicationController to include Pundit for authorization.
    - Refactored ApplicationPolicy to align with Pundit's structure and added necessary methods for policy management.

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -1,8 +1,53 @@
-# Base class for application policies
-class ApplicationPolicy < ActionPolicy::Base
-  private
+# Base policy for the application (Pundit)
+class ApplicationPolicy
+  attr_reader :user, :record
 
-  def owner?
-    record.user_id == user.id
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    false
+  end
+
+  def create?
+    false
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    false
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    false
+  end
+
+  # Scope policy for the application (Pundit)
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      raise NoMethodError, "You must define #resolve in #{self.class}"
+    end
+
+    private
+
+    attr_reader :user, :scope
   end
 end
