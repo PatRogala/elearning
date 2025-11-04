e0af631028f9cccc370538287562c77d8363c143
Patryk Rogala <patryk@astarium.pl>
2025-11-04 10:41:03 +0100

    Configure VCR and WebMock for improved test environment
    - Updated VCR configuration to ignore localhost requests.
    - Enabled WebMock to disable all external network connections during tests.

--- config/initializers/vcr.rb
+++ config/initializers/vcr.rb
@@ -2,5 +2,6 @@ return unless Rails.env.test?
 
 VCR.configure do |config|
   config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.ignore_localhost = true
   config.hook_into :webmock
 end
