74ef95dc355f63be317daa426ce6787b1426c4de
Patryk Rogala <patryk@astarium.pl>
2025-12-07 23:04:42 +0100

    Implement Pundit authorization handling in ApplicationController
    - Added rescue_from for Pundit::NotAuthorizedError to handle unauthorized access.
    - Introduced user_not_authorized method to set flash alert and redirect users.
    - Created Polish locale file for unauthorized flash message.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,13 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   after_action :verify_authorized, unless: :devise_controller?
+  around_action :switch_locale
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  around_action :switch_locale
 
   def switch_locale(&)
     locale = params[:locale] || I18n.default_locale
@@ -16,5 +17,10 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     { locale: I18n.locale }
+  end
+
+  def user_not_authorized
+    flash[:alert] = t("flash.unauthorized")
+    redirect_to(request.referer || root_path)
   end
 end
