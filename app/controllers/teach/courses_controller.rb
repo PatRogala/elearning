d620209cf45ff347f0fc6d54226c3f719e606a95
Patryk Rogala <patryk@astarium.pl>
2025-12-08 08:25:41 +0100

    Add CoursesController and request specs for course creation
    - Created CoursesController to handle course creation for instructors, including authentication and authorization.
    - Added request specs to test the new course creation functionality, ensuring proper redirection for unauthenticated users and non-teachers, as well as successful access for authenticated teachers.

---
+++ app/controllers/teach/courses_controller.rb
@@ -0,0 +1,11 @@
+module Teach
+  # Controller for teaching courses
+  class CoursesController < ApplicationController
+    before_action :authenticate_user!
+
+    def new
+      @course = Course.new
+      authorize([:instructor, @course])
+    end
+  end
+end
