f577c74c3816910bb939d8d2c079411ef250337b
Patryk Rogala <patryk@astarium.pl>
2025-12-17 09:20:56 +0100

    validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,6 +8,8 @@ class Course < ApplicationRecord
   validates :title, uniqueness: { scope: :instructor_id }
   validates :image, content_type: ACCEPTED_IMAGE_CONTENT_TYPES
   validates :image, size: { less_than_or_equal_to: 5.megabytes }
+  validates :price_cents, presence: true
+  validates :price_currency, presence: true
 
   has_rich_text :description
   has_one_attached :image do |attachable|
