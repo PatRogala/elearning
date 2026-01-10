29649229787e1e57e6fe6ec2f370c1347de85027
Patryk Rogala <patryk@astarium.pl>
2026-01-10 23:01:14 +0100

    Enhance profile edit form with avatar upload functionality, localization updates, and supporting controller & spec changes.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,22 @@
 require "rails_helper"
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "PATCH /update" do
+    let(:user) { create(:user) }
+    before { sign_in(user) }
+
+    context "when params are valid and user is editing own profile" do
+      it "updates user" do
+        patch user_path(id: user.id), params: { user: { first_name: "John" } }
+        expect(user.reload.first_name).to eq("John")
+      end
+    end
+
+    context "when params are invalid" do
+      it "does not update user" do
+        patch user_path(id: user.id), params: { user: { first_name: "" } }
+        expect(user.reload.first_name).not_to be_empty
+      end
+    end
   end
 end
