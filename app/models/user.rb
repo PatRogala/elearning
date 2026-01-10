4b4e9b142b73d76ca0b6ad4c33d973cd6e42c507
Patryk Rogala <patryk@astarium.pl>
2026-01-10 22:31:22 +0100

    Add avatar support to User model and update profile edit form

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   has_many :roles, through: :user_roles
   has_many :created_courses, class_name: "Course", foreign_key: "instructor_id", dependent: :destroy,
                              inverse_of: :instructor
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [128, 128]
+    attachable.variant :preview, resize_to_limit: [1000, 1000]
+  end
 
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -30,5 +34,17 @@ class User < ApplicationRecord
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def avatar_preview
+    return avatar.variant(:preview) if avatar.attached?
+
+    placeholder_image
+  end
+
+  private
+
+  def placeholder_image
+    "courses/placeholder_pattern.png"
   end
 end
