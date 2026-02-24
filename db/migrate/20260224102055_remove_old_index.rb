3321d793050fc94a657bcad56c494ffe676500ba
Patryk Rogala <patryk@astarium.pl>
2026-02-24 12:40:29 +0100

    if exists

--- db/migrate/20260224102055_remove_old_index.rb
+++ db/migrate/20260224102055_remove_old_index.rb
@@ -1,6 +1,6 @@
 # Remove duplicated index, we use lower(name) index instead
 class RemoveOldIndex < ActiveRecord::Migration[8.1]
   def change
-    remove_index :roles, :name
+    remove_index :roles, :name, if_exists: true
   end
 end
