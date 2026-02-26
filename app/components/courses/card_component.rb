02b4c4fbd9b84d678d8c2663d336b6fe2f2e5902
Patryk Rogala <patryk@astarium.pl>
2026-02-26 15:03:26 +0100

    Add Course Card Component and Controller
    - Introduced a new CardComponent for rendering course cards with details such as title, description, instructor, and price.
    - Updated the library_component to utilize the new CardComponent for displaying courses.
    - Created CoursesController to handle course index actions, ensuring proper authorization and data retrieval.
    - Added CoursePolicy to manage access permissions for viewing courses.
    - Updated routes to include courses resource for index action.
    - Created index view to render the course catalog.

---
+++ app/components/courses/card_component.rb
@@ -0,0 +1,11 @@
+module Courses
+  # Component for rendering a course card
+  class CardComponent < ApplicationViewComponent
+    param :course, Types::Instance(Course)
+
+    delegate :title, to: :course
+    delegate :description, to: :course
+    delegate :instructor, to: :course
+    delegate :image_cover, to: :course
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
     devise_for :users
 
     resources :users, only: %i[update]
+    resources :courses, only: %i[index]
 
     # Teach routes
     namespace :teach do
