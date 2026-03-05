a765c72bb1b0e452ac770e219cff43e73de99fa4
Patryk Rogala <patryk@astarium.pl>
2026-03-05 21:37:16 +0100

    Refactor Logtail logger initialization in production.rb
    - Updated the Logtail logger configuration to check for the presence of the source token before initialization.
    - Enhanced code clarity while maintaining functionality.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,10 +35,12 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [:request_id]
-  config.logger = Logtail::Logger.create_default_logger(
-    Rails.application.credentials.dig(:logtail, :source_token),
-    ingesting_host: Rails.application.credentials.dig(:logtail, :ingesting_host)
-  )
+  if (logtail_token = Rails.application.credentials.dig(:logtail, :source_token))
+    config.logger = Logtail::Logger.create_default_logger(
+      logtail_token,
+      ingesting_host: Rails.application.credentials.dig(:logtail, :ingesting_host)
+    )
+  end
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!).
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
