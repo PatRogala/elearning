72c9e31ed9504aa3195a34df53f3d06835c23a20
Patryk Rogala <patryk@astarium.pl>
2026-03-06 11:55:12 +0100

    Refactor CoursePolicy specs to include user creation and scope tests
    - Updated the CoursePolicy spec to create a user using FactoryBot.
    - Implemented tests for the scope method to return all published courses.
    - Added a test for the index permission to ensure proper authorization for the Course model.

--- spec/policies/course_policy_spec.rb
+++ spec/policies/course_policy_spec.rb
@@ -3,25 +3,23 @@ require "rails_helper"
 RSpec.describe CoursePolicy, type: :policy do
   subject { described_class }
 
-  let(:user) { User.new }
+  let(:scope) { CoursePolicy::Scope.new(user, Course).resolve }
+
+  let(:user) { create(:user) }
 
   permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+    before do
+      Prosopite.pause
+      create_list(:course, 3, :published)
+      Prosopite.resume
+    end
+
+    it "returns all published courses" do
+      expect(scope).to match_array(Course.all)
+    end
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+  permissions :index? do
+    it { is_expected.to permit(user, Course) }
   end
 end
