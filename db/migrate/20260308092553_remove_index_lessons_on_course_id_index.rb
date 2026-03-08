821c0bf97a07a985180abc98763f39c49e8c3986
Patryk Rogala <patryk@astarium.pl>
2026-03-08 10:48:14 +0100

    rubocop

--- db/migrate/20260308092553_remove_index_lessons_on_course_id_index.rb
+++ db/migrate/20260308092553_remove_index_lessons_on_course_id_index.rb
@@ -1,6 +1,6 @@
 # Duplicated index index_lessons_on_course_id_and_position covers it
 class RemoveIndexLessonsOnCourseIdIndex < ActiveRecord::Migration[8.1]
-  def change
+  def down
     remove_index "lessons", name: "index_lessons_on_course_id"
   end
 end
