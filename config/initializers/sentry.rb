f3f88e636cc35924ad6e451e97ac0169d8afbb29
Patryk Rogala <patryk@astarium.pl>
2026-03-15 14:24:31 +0100

    Refactor Sentry DSN configuration for improved readability
    - Updated the Sentry initializer to use `dig` method for accessing the DSN from credentials, enhancing clarity and maintainability.

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 # Sentry config for error tracking via Better Stack
 Sentry.init do |config|
-  config.dsn = Rails.application.credentials.sentry[:dsn]
+  config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
 
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
