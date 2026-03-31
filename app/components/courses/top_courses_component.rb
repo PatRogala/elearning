b3000b89346bbc15f0f57989de740a44e80221e7
Patryk Rogala <patryk@astarium.pl>
2026-03-31 10:37:00 +0200

    Refactor TopCoursesComponent to use localized strings and improve course fetching logic. Added English and Polish locale files for component text.

--- app/components/courses/top_courses_component.rb
+++ app/components/courses/top_courses_component.rb
@@ -1,4 +1,12 @@
 module Courses
-  class TopCoursesComponent < ViewComponent::Base
+  # Show top courses on landing page top section
+  class TopCoursesComponent < ApplicationViewComponent
+    def courses
+      @courses ||= Course.published.recent.limit(6).with_attached_image.includes(:instructor)
+    end
+
+    def render?
+      courses.any?
+    end
   end
 end
