0d1823dabc9e3e57100394d0c303b8a904a5a779
Patryk Rogala <patryk@astarium.pl>
2026-04-05 14:48:12 +0200

    Refactor top courses component for improved layout and translations
    - Updated `top_courses_component.html.haml` to enhance the layout and styling of the courses section, including new classes for better responsiveness and visual appeal.
    - Modified the Ruby component to include rich text descriptions and instructor avatar attachments for a more informative display.
    - Updated English and Polish locale files to change "Top courses" to "Newest courses" and "Browse all modules" to "Browse all courses," along with adding a new title and start learning text.
    - Adjusted component tests to reflect the updated content and ensure proper rendering.
    
    These changes aim to create a more engaging and user-friendly experience for users exploring the top courses.

--- app/components/courses/top_courses_component.rb
+++ app/components/courses/top_courses_component.rb
@@ -2,7 +2,12 @@ module Courses
   # Show top courses on landing page top section
   class TopCoursesComponent < ApplicationViewComponent
     def courses
-      @courses ||= Course.published.recent.limit(6).with_attached_image.includes(:instructor)
+      @courses ||= Course.published
+                         .recent
+                         .limit(6)
+                         .with_attached_image
+                         .with_rich_text_description
+                         .includes(instructor: { avatar_attachment: :blob })
     end
 
     def render?
