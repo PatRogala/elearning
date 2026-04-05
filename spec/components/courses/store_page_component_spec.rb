d610e251fb643ccd7ceaaadf9d8ad3915e4b6df4
Patryk Rogala <patryk@astarium.pl>
2026-04-05 21:59:59 +0200

    Refactor store page component with enhanced styling and localization updates
    - Redesigned the layout of the `store_page_component.html.haml` to improve visual appeal and user experience, incorporating new background elements and responsive design adjustments.
    - Updated localization files for both English and Polish, adding new keys and improving existing translations to enhance user guidance.
    - Adjusted test expectations in `store_page_component_spec.rb` to reflect updated content, ensuring accuracy in the testing process.
    
    These changes aim to create a more engaging and user-friendly interface for the course store page.

--- spec/components/courses/store_page_component_spec.rb
+++ spec/components/courses/store_page_component_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe Courses::StorePageComponent, type: :component do
     expect(page).to have_content(course.title)
       .and have_content(course.description.to_plain_text.truncate(100))
       .and have_content(course.instructor.full_name)
-      .and have_content(course.price)
+      .and have_content("Free")
   end
 end
